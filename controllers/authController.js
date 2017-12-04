7c9b59223b4730f57ca44f67069ec4477ac89fbd
Ankit Tiwari <ankitt255@gmail.com>
2017-12-04 15:29:49 +0530

    User must be logged in to add a store

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,14 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
 };
+
+exports.isLoggedIn = (req, res) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // carry on! They are loggen in!
+    return;
+  }
+
+  req.flash('error', 'Oops you must be logged in to do that!');
+  res.redirect('/login');
+};
