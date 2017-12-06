4ecbae14beb3dbcff2dee4fc25a6797e29867471
Ankit Tiwari <ankitt255@gmail.com>
2017-12-06 21:19:38 +0530

    Show review form

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -57,7 +58,11 @@ router.get(
   authController.isLoggedIn,
   catchErrors(storeController.getHearts)
 );
-
+router.post(
+  '/reviews/:id',
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
 /*
   API
 */
